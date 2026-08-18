92a60f136bc3b729dc554c64d3290be38bc8d2bc
Rayane Van Osselt <vanosselt.rayane@gmail.com>
2026-08-18 09:32:58 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -96,6 +96,7 @@ const CONFIG = {
     "2026-08-23",   // Indisponible (personnel)
     "2026-08-17"    // Indisponible (personnel)
     // "2026-07-21",  // ← exemple : ajoutez vos propres dates ici
+"2026-08-18",
   ],
 
   /* -------------------------------------------------------------------
